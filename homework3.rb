2650cb0f5282faa92db81af520543b57474a89c6
kristina.argirova <kristina.argirova@betlab.com>
2016-02-18 00:07:29 +0200

    formatting fixed

--- homework3.rb
+++ homework3.rb
@@ -34,54 +34,56 @@ class Developer
 				raise "Нечего делать!"
 			end
 		rescue
-
 		end
-    end
-		def status
-			case @@list.length
-				when (1..MAX_TASKS)
-					puts "работаю"
-				when 0
-					puts "свободен"
-				else
-					puts "занят"
-			end
-		end
+  end
 
-		def can_add_task?
-			@@list.length.between?(0,MAX_TASKS-1)
-		end
-
-		def can_work?
-			@@list.length.zero?
-		end
-	end
-
-	class JuniorDeveloper < Developer
-		MAX_TASKS = 5
-
-		def work!
-		  begin
-		    @@list.each{ |task| raise "Слишком сложно!" if @@list.chars.delete_if{|char| char == ' '}.length > 20 }
-		    binding.pry
-		  rescue
-		  	puts "Слишком сложно!"
-		  end
-
-		  puts "Вася: пытаюсь делать задачу #{@@list.shift}. Осталось задач: #{@@list.length}" if @@list.length > 0
-	    end
-end
-	class SeniorDeveloper < Developer
-		MAX_TASKS = 15
-
-		def work!
-			if @@list.length > 0
-				puts "Вася: пытаюсь делать задачу #{@@list.shift}. Осталось задач: #{@@list.length}"
+	def status
+	  case @@list.length
+			when (1..MAX_TASKS)
+				puts "работаю"
+			when 0
+				puts "свободен"
 			else
-				puts "Что-то лень"
+				puts "занят"
 			end
+	end
+
+	def can_add_task?
+		@@list.length.between?(0,MAX_TASKS-1)
+	end
+
+	def can_work?
+		@@list.length.zero?
+	end
+end
+
+class JuniorDeveloper < Developer
+	MAX_TASKS = 5
+
+	def work!
+		begin
+      @@list.each{ |task| raise "Слишком сложно!" if @@list.chars.delete_if{|char| char == ' '}.length > 20 }
+    binding.pry
+		rescue
+		  puts "Слишком сложно!"
+		end
+
+		puts "Вася: пытаюсь делать задачу #{@@list.shift}. Осталось задач: #{@@list.length}" if @@list.length > 0
+	end
+
+end
+
+class SeniorDeveloper < Developer
+	MAX_TASKS = 15
+
+	def work!			
+		if @@list.length > 0
+			puts "Вася: пытаюсь делать задачу #{@@list.shift}. Осталось задач: #{@@list.length}"
+		else				
+			puts "Что-то лень"
 		end
 	end
+end
 
 
 	dev = JuniorDeveloper.new('Вася')
